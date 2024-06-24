b14ffd63f1bcf504554429f9c4d5037a7a8b8857
Sarathjnana <119930337+Sarathjnana@users.noreply.github.com>
2024-06-24 15:33:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const externalModule = require('external-module'); // Example of using an external npm module
 
 exports.handler = async (event) => {
-    console.log('Hello World');
+    console.log('Hello World jnana');
     return 'Hello World';
 };
 
