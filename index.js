b0dc1df572261515d8874e8e19898b8eadfac1f2
jnanasarath <sarath@nhrtechnologies.com>
2024-06-24 14:55:52 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+// index.js
+const externalModule = require('external-module'); // Example of using an external npm module
+
+exports.handler = async (event) => {
+    console.log('Hello World');
+    return 'Hello World';
+};
+
